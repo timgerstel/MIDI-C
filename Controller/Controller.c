0b6e58b42bf97d9d0196e1f885c40dc526a87fb7
Xcoder62 <cmendes062@gmail.com>
2017-10-13 16:50:44 -0400

    Copied skeleton

--- Controller/Controller.c
+++ Controller/Controller.c
@@ -1,11 +1,33 @@
 #include<io.h>
 #include<util/delay.h>
+
+//PB4 = record
+//PB2 = PLAY
+// PB1 = Modify
 int main(void){
-    DDRB = 0xFF;
+    DDRB = 0xFF;  //Set outputs
+	DDRA = 0x00;  //Set inputs
+	PORTB = 0x00; //Turns all leds off
+	PORTA = 0x07; // sets inputs to return 5v on PA0, PA1, PA2
     while(1){
-	for (uint8_t bval = 0; bval <= 255; bval++){
-	    _delay_ms(20);
-	    PORTB = bval;
-	}
+	
+	if((PINA & 0x04) && !(PINA & 0x02)) //Record Mode
+		{
+			PORTB = 0x01; 
+		}
+	if((PINA & 0x02) && !(PINA & 0x04)) // Play Mode
+		{ 	
+			if (PINA & 0x01){ // Modify Mode
+				PORTB = 0x04;
+			}else{
+				PORTB = 0x02;
+			}	
+		}
+	
+	 
+		else // if all off, leds are off
+		{
+		PORTB = 0x00;
+		}
     }
 }
